bd45215c3f3cc3d4f3ee0dd67ab7b311e8bff562
misterEarly <afi.lovisfrueh@gmail.com>
2022-03-11 09:29:03 +0000

    added Encoder Counter include and initialising

--- main.cpp
+++ main.cpp
@@ -8,19 +8,28 @@
 
 #include "IIR_filter.h"
 #include "rt_loop.h"
+#include "EncoderCounter.h"
 
 DigitalOut led(LED1);
 
 // main program
 static BufferedSerial serial_port(USBTX, USBRX);
 
+EncoderCounter enc1(PA_6, PC_7);
+
 int main()
 {    serial_port.set_baud(115200);
     serial_port.set_format(8, BufferedSerial::None, 1);
     serial_port.set_blocking(false);
+
+    
+
+
     printf("Start loop\n");
+
     rt_loop my_loop(.10);
     my_loop.start_theloop();
+
     while(1) 
     ;    
 }
